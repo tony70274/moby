3c95de255a68479b20394731effcd5c92e958d0d
Tung-i <tony702742000@gmail.com>
2018-07-02 18:59:25 +0800

    0702-moby

--- daemon/FDSAlloc.go
+++ daemon/FDSAlloc.go
@@ -2,11 +2,18 @@ package daemon
 
 import (
 //	"context"
-//	"fmt"
+	"fmt"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/container"
+	"text/tabwriter"
+	//"time"
 //	"github.com/docker/docker/api/types/container"
 //	"github.com/docker/docker/errdefs"
 //	"github.com/pkg/errors"
+	"os/exec"
+	"os"
+	//"time"
+	"time"
 )
 
 func (daemon *Daemon) ContainerFDS(policy *types.ContainerFDSOptions) error {
@@ -27,8 +34,61 @@ func (daemon *Daemon) ContainerFDS(policy *types.ContainerFDSOptions) error {
 		return container.ContainerUpdateOKBody{Warnings: warnings}, err
 	}
 */
+	var containerInfo []*container.Container
+	containers ,err := daemon.Containers(&types.ContainerListOptions{})
+	if err != nil{
+		println("Get ContainerList error")
+		return nil
+	}else{
+		println("Get ContainerList")
+	}
+	for _, container := range containers{
+		tmp, err := daemon.GetContainer(container.ID)
+		//println(&tmp)
+		println(tmp.HostConfig.Resources.CPUPeriod)
+		if err != nil{
+			println("Get ContainerInfo error")
+			return nil
+		}else{
+			println("Get ContainerInfo")
+		}
+		containerInfo = append(containerInfo,tmp)
+		println("Info: ",&containerInfo)
+
+	}
+	println("Info",&containerInfo)
+	//show(containerInfo)
+	//sshowInfo(containerInfo)
+	for range time.Tick(time.Millisecond * 1000){
+		//cleanScreen()
+		showInfo(containerInfo)
+	}
     println("In the func of ContainerFDS from Daemon.")
     println("Policy is ",policy.Policy)
 
 	return nil
+}
+func showInfo(cs []*container.Container){
+	println(&cs)
+	w := tabwriter.NewWriter(os.Stdout, 12, 1 ,3 ,' ',0 )
+	//fmt.Fprint(w, "Name\tCPU%\tAVG\tQuota\tPeriod\tMax_CPU\n")
+	fmt.Fprint(w, "Name\tPeriod\tQuota\n")
+	for _, c := range cs{
+		fmt.Fprintf(w, "%s\n",
+			c.Name,
+			c.HostConfig.CPUPeriod,
+			c.HostConfig.CPUQuota,
+		)
+	}
+
+
+
+}
+func show(cs []*container.Container){
+	println(&cs)
+}
+func cleanScreen(){
+	cmd := exec.Command("Clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
